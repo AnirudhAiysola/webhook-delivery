d1fecdb87ea5a1c89522e13d4c5a697acf5c169d
anirudhaiysola <anirudhisola1011@gmail.com>
2026-07-26 20:28:20 -0500

    db fix

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,10 @@
 import { Pool } from "pg";
 
+const useSSL = process.env.DATABASE_URL?.includes("rds.amazonaws.com");
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: useSSL ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("error", (err) => {
